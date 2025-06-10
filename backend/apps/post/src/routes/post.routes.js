607ec5b5adb88d702cc9fb0ed06f9eeae6d879db
alain-007 <fouetialain@gmail.com>
2025-06-10 22:12:59 +0200

    delete like & dislike

--- backend/apps/post/src/routes/post.routes.js
+++ backend/apps/post/src/routes/post.routes.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { setPosts, getPosts, editPost,likePost,deletePost,dislikePost } = require("../controllers/post.controllers");
+const { setPosts, getPosts, editPost,deletePost } = require("../controllers/post.controllers");
 
 //Appel de la fonfoction router sur la constante router
 const router=express.Router();
@@ -8,8 +8,8 @@ const router=express.Router();
 router.get("/",getPosts);
 router.post("/",setPosts);
 router.put("/:id",editPost);
-router.patch("/like-post/:id", likePost);
-router.patch("/dislike-post/:id", dislikePost);
+//router.patch("/like-post/:id", likePost);
+//router.patch("/dislike-post/:id", dislikePost);
 router.delete("/:id", deletePost);
 
 // router.get("/",(req,res)=>{
