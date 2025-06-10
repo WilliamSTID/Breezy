59394609d2904986e3ecec4c40d1ad0555d782ce
Djibril DABO <djibrildabo@MacBook-Air-de-Djibril.local>
2025-06-10 20:29:22 +0200

    Correstion du fichier Post.js dans models

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, default: "" }, // Titre optionnel
   content: { type: String, required: true },
-  // Ajoute d'autres champs si besoin (ex: title, images, etc.)
+  images: [{ type: String }], // Tableau d'URL d'images
+  videos: [{ type: String }], // Tableau d'URL de vidéos
 }, { timestamps: true });
 
 module.exports = mongoose.model('Post', PostSchema);
