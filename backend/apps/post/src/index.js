4cba9a83a9fa157092384405093c0a3946699f5d
alain-007 <fouetialain@gmail.com>
2025-06-12 12:01:05 +0200

    Adding latest files

--- backend/apps/post/src/index.js
+++ backend/apps/post/src/index.js
@@ -1,15 +1,36 @@
 const express=require("express");
-
 const dotenv=require("dotenv");
+const mongoose=require('mongoose');
 const applyMiddlewares = require("./middlewares/post.middlewares");
 dotenv.config();
-const connectDB = require("../../../post.db.js");
 const app=express();
-const port=3000;
+const port = process.env.PORT||3000; 
+const MONGO_URI = process.env.MONGO_URI;
+
 
 
 
 //renseigner au seveur la connexion à la BDD MongoDB
+
+//connexion avec la BDD mongoD via la bibliothèque Mongoose
+
+
+
+
+//fonction asynchrone pour recup info vers le serveur distant
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+     
+    });
+
+    console.log(' MongoDB connecté avec succès');
+  } catch (err) {
+    console.error(' Erreur de connexion MongoDB :', err.message);
+    process.exit(1); 
+  }
+};
+
 connectDB();
 
 // Application des middlewares
