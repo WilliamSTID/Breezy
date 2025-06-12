168c29c89d5c65188a356c3f1bac43d3a19b3047
WilliamSTID <113010589+WilliamSTID@users.noreply.github.com>
2025-06-12 12:15:46 +0200

    Ajout modification post

--- backend/apps/post/src/index.js
+++ backend/apps/post/src/index.js
@@ -4,45 +4,22 @@ const mongoose=require('mongoose');
 const applyMiddlewares = require("./middlewares/post.middlewares");
 dotenv.config();
 const app=express();
-const port = process.env.PORT||3000; 
-const MONGO_URI = process.env.MONGO_URI;
-
-
-
-
-//renseigner au seveur la connexion à la BDD MongoDB
-
-//connexion avec la BDD mongoD via la bibliothèque Mongoose
-
-
-
-
-//fonction asynchrone pour recup info vers le serveur distant
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-     
-    });
-
-    console.log(' MongoDB connecté avec succès');
-  } catch (err) {
-    console.error(' Erreur de connexion MongoDB :', err.message);
-    process.exit(1); 
-  }
-};
-
-connectDB();
+const PORT = process.env.PORT||3000;
 
 // Application des middlewares
 applyMiddlewares(app);
 
-
-
 //spécification des routes à aller récupérer
 app.use("/post",require("./routes/post.routes"));
 
-
-
-//lancement du serveur
-app.listen(port,()=>console.log(`Serveur lancé sur port ${port}`));
-
+//connexion avec la BDD mongoD via la bibliothèque Mongoose
+mongoose
+    .connect( process.env.MONGO_URI || 'mongodb://localhost:27017/breezy')
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`post service running on port ${PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error('Erreur lors de la connexion à MongoDB :', err);
+    });
